98a4bcf90012c1effd6d9970593adf5b2ab7530f
nahanoo <ericsven.u@gmail.com>
2025-12-19 12:11:00 +0100

    added multithreading cleaned code

--- isoclines.py
+++ isoclines.py
@@ -1,4 +1,4 @@
-from model import Model
+from model_chain import Model
 from experiment import Species
 import numpy as np
 from style import *
@@ -6,14 +6,17 @@ import plotly.graph_objects as go
 import pandas as pd
 from os import path
 from multiprocessing import Pool
+from plotly.subplots import make_subplots
 
 font_size = 14
 
 
 def simulate_endpoints(args):
     conc, params, xs, species_name, a = args
+
     at = Species("At", params.loc["At"])
     oa = Species("Oa", params.loc["Oa"])
+
     if species_name == "At":
         oa.N0 = 0.0
         at.a = a
@@ -21,11 +24,21 @@ def simulate_endpoints(args):
         at.N0 = 0.0
         oa.a = a
     elif species_name == "At+Oa":
-        at.a = a[0]
-        oa.a = a[1]
+        at.a, oa.a = a
+
     model = Model(at, oa, None, xs, conc, 0.3)
     model.integrate_model()
-    return model.succinate[-1], model.glucose[-1], model.at.y[-1], model.oa.y[-1]
+
+    return (
+        model.c1.succinate[-1],
+        model.c1.glucose[-1],
+        model.c1.at_y[-1],
+        model.c1.oa_y[-1],
+        model.c2.succinate[-1],
+        model.c2.glucose[-1],
+        model.c2.at_y[-1],
+        model.c2.oa_y[-1],
+    )
 
 
 def a_mono_culture():
@@ -37,21 +50,25 @@ def a_mono_culture():
     with Pool() as pool:
         args_at = [(conc, params, xs, "At", a) for a in aas]
         results_at = pool.map(simulate_endpoints, args_at)
-        C1s_at, C2s_at, At_end_at, Oa_end_at = zip(*results_at)
+        suc_at_c1, gluc_at_c1, at_c1, oa_c1, suc_at_c2, gluc_at_c2, at_c2, oa_c2 = (
+            np.asarray(results_at).T
+        )
         args_oa = [(conc, params, xs, "Oa", a) for a in aas]
         results_oa = pool.map(simulate_endpoints, args_oa)
-        C1s_oa, C2s_oa, At_end_oa, Oa_end_oa = zip(*results_oa)
+        suc_oa_c1, gluc_oa_c1, _, oa_c1, suc_oa_c2, gluc_oa_c2, _, oa_c2 = np.asarray(
+            results_oa
+        ).T
 
     fig = go.Figure()
     fig.add_trace(
         go.Scatter(
-            x=C1s_at,
-            y=C2s_at,
+            x=suc_at_c1,
+            y=gluc_at_c1,
             mode="markers",
             marker=dict(
                 symbol="triangle-up",
                 color=aas,
-                colorscale="viridis",
+                colorscale="cividis",
                 colorbar=dict(
                     title="a",
                     len=0.55,  # height as fraction of plotting area
@@ -67,12 +84,12 @@ def a_mono_culture():
 
     fig.add_trace(
         go.Scatter(
-            x=C1s_oa,
-            y=C2s_oa,
+            x=suc_oa_c1,
+            y=gluc_oa_c1,
             mode="markers",
             marker=dict(
                 color=aas,
-                colorscale="viridis",
+                colorscale="cividis",
                 showscale=False,
             ),
             line=dict(width=2),
@@ -91,96 +108,74 @@ def a_mono_culture():
         title="Mono-culture",
     )
     fig = style_plot(fig, line_thickness=3, marker_size=10, font_size=font_size)
-    fig.write_image("plots/contours/a_C1_C2_resource_space2.pdf")
+    fig.write_image("plots/contours/mono_culture_resource_allocation.svg")
 
 
-def a1_a2_sweep():
+def resource_allocation_heatmap():
+    colors_heatmap = [
+        colors["Glucose"],
+        colors["Succinate"],
+    ]
+    aas = np.linspace(0.0, 1.0, 20)
     conc = 15
     p_f = path.join("parameters", f"parameters_{conc}_mM_C.csv")
     params = pd.read_csv(p_f, index_col=0)
-
     xs = np.linspace(0, 100, 1000)
-    aas = np.linspace(0.0, 1.0, 50)
+    with Pool() as pool:
+        args = [
+            (conc, params, xs, "At+Oa", (a_at, a_oa)) for a_at in aas for a_oa in aas
+        ]
+        results = pool.map(simulate_endpoints, args)
+        suc_c1, gluc_c1, at_c1, oa_c1, suc_c2, gluc_c2, at_c2, oa_c2 = np.asarray(
+            results
+        ).T
 
+    fig = make_subplots(
+        rows=1,
+        cols=2,
+        subplot_titles=("First chemostat", "Downstream chemostat"),
+        horizontal_spacing=0.01,
+        shared_xaxes=True,
+        shared_yaxes=True,
+    )
     n = len(aas)
-    Z_C1 = np.empty((n, n))
-    Z_C2 = np.empty((n, n))
-    Z_N1 = np.empty((n, n))
-    Z_N2 = np.empty((n, n))
+    at_c1_grid, oa_c1_grid = at_c1.reshape(n, n).T, oa_c1.reshape(n, n).T
+    z_c1 = np.log10(at_c1_grid / oa_c1_grid)
+    at_c2_grid, oa_c2_grid = at_c2.reshape(n, n).T, oa_c2.reshape(n, n).T
+    z_c2 = np.log10(at_c2_grid / oa_c2_grid)
+    fig.add_trace(
+        go.Heatmap(
+            x=aas,
+            y=aas,
+            z=z_c1,
+            colorscale="cividis",
+            colorbar=dict(
+                title="log<sub>10</sub>(At/Oa)",
+            ),
+        ),
+        row=1,
+        col=1,
+    )
 
-    for at_a in aas:
-        for oa_a in aas:
-            args = (conc, params, xs, "At+Oa", (at_a, oa_a))
-            C1m, C2m, N1m, N2m = simulate_endpoints(args)
-            j = np.where(aas == at_a)[0][0]
-            i = np.where(aas == oa_a)[0][0]
-            Z_C1[i, j] = C1m
-            Z_C2[i, j] = C2m
-            Z_N1[i, j] = N1m
-            Z_N2[i, j] = N2m
+    fig.add_trace(
+        go.Heatmap(
+            x=aas,
+            y=aas,
+            z=z_c2,
+            colorscale="cividis",
+            showscale=False,
+        ),
+        row=1,
+        col=2,
+    )
 
-    ratio = Z_C1 / (Z_C1 + Z_C2)
-
-    Zs = [Z_C1, Z_C2, ratio]
-    scale_names = ["Succinate<br>[mM]", "Glucose<br>[mM]", "Succinate<br>ratio"]
-    zmaxs = [np.nanmax(Z_C1), np.nanmax(Z_C2), 1.0]  # ratio in [0,1]
-    filenames = ["a1_a2_C12.pdf", "a1_a2_C22.pdf", "a1_a2_ratio2.pdf"]
-
-    for idx, Z in enumerate(Zs):
-        fig = go.Figure()
-        fig.add_trace(
-            go.Contour(
-                z=Z,
-                x=aas,
-                y=aas,
-                contours=dict(showlines=False),
-                colorscale="Viridis",
-                zmin=0,
-                zmax=zmaxs[idx],
-                zauto=False,
-                ncontours=50,
-                colorbar=dict(title=scale_names[idx], len=0.6, y=0.2, thickness=12),
-                name=scale_names[idx],
-            )
-        )
-
-        fig.update_layout(
-            xaxis=dict(title="a At", ticks="inside"),
-            yaxis=dict(title="a Oa", ticks="inside"),
-        )
-        fig = style_plot(fig, line_thickness=1.5, font_size=font_size)
-        fig.write_image(f"plots/contours/{filenames[idx]}")
-
-    ratio = Z_N1 / (Z_N1 + Z_N2)
-    Zs = [Z_N1, Z_N2, ratio]
-    scale_names = ["OD At", "OD Oa", "At ratio"]
-    zmaxs = [np.nanmax(Z_N1), np.nanmax(Z_N2), 1.0]  # ratio in [0,1]
-    filenames = ["a1_a2_C12_At.pdf", "a1_a2_C22_Oa.pdf", "a1_a2_ratio2_At_Oa.pdf"]
-
-    for idx, Z in enumerate(Zs):
-        fig = go.Figure()
-        fig.add_trace(
-            go.Contour(
-                z=Z,
-                x=aas,
-                y=aas,
-                contours=dict(showlines=False),
-                colorscale="Viridis",
-                zmin=0,
-                zmax=zmaxs[idx],
-                zauto=False,
-                ncontours=50,
-                colorbar=dict(title=scale_names[idx], len=0.6, y=0.2, thickness=12),
-                name=scale_names[idx],
-            )
-        )
-
-        fig.update_layout(
-            xaxis=dict(title="a At", ticks="inside"),
-            yaxis=dict(title="a Oa", ticks="inside"),
-        )
-        fig = style_plot(fig, line_thickness=1.5, font_size=font_size)
-        fig.write_image(f"plots/contours/{filenames[idx]}")
-
-
-a_mono_culture()
+    fig.update_layout(
+        xaxis_title="Resource allocation At",
+        xaxis2_title="Resource allocation At",
+        yaxis_title="Resource allocation Oa",
+        width=750,
+        height=380,
+        title="Coexistence in succinate-glucose space",
+    )
+    fig = style_plot(fig, font_size=12, line_thickness=2, top_margin=35)
+    fig.write_image("plots/contours/coexistence_resource_allocation.svg")
