f09e640ad20b3871cfb928ef90f570e07954c89b
nahanoo <ericsven.u@gmail.com>
2026-01-19 14:28:46 +0100

    tried to get analytical solutions

--- equations.py
+++ equations.py
@@ -12,6 +12,11 @@ oa = Species("Oa", params.loc["Oa"])
 D = 0.3
 
 (
+    at_c1_a,
+    at_c2_a,
+    a,
+    oa_c1_a,
+    oa_c2_a,
     at_c1,
     oa_c1,
     s_c1,
@@ -25,26 +30,26 @@ D = 0.3
     J_at_c2,
     J_oa_c2,
 ) = symbols(
-    "at_c1 oa_c1 s_c1 g_c1 at_c2 oa_c2 s_c2 g_c2 J_at_c1 J_oa_c1 J_at_c2 J_oa_c2"
+    "at_c1_a at_c2_a a oa_c1_a oa_c2_a at_c1 oa_c1 s_c1 g_c1 at_c2 oa_c2 s_c2 g_c2 J_at_c1 J_oa_c1 J_at_c2 J_oa_c2"
 )
 
 eq_J_at_c1 = Eq(
     J_at_c1,
-    at.a * at.v_gluc * g_c1 / (at.K_gluc + g_c1)
-    + (1 - at.a) * at.v_succ * s_c1 / (at.K_succ + s_c1),
+    at_c1_a * at.v_gluc * g_c1 / (at.K_gluc + g_c1)
+    + (1 - at_c1_a) * at.v_succ * s_c1 / (at.K_succ + s_c1),
 )
 eq_J_oa_c1 = Eq(
     J_oa_c1,
-    oa.a * oa.v_gluc * g_c1 / (oa.K_gluc + g_c1)
-    + (1 - oa.a) * oa.v_succ * s_c1 / (oa.K_succ + s_c1),
+    oa_c1_a * oa.v_gluc * g_c1 / (oa.K_gluc + g_c1)
+    + (1 - oa_c1_a) * oa.v_succ * s_c1 / (oa.K_succ + s_c1),
 )
 eq_J_at_c2 = Eq(
     J_at_c2,
-    at.a * at.v_gluc * g_c2 / (at.K_gluc + g_c2)
-    + (1 - at.a) * at.v_succ * s_c2 / (at.K_succ + s_c2),
+    at_c2_a * at.v_gluc * g_c2 / (at.K_gluc + g_c2)
+    + (1 - at_c2_a) * at.v_succ * s_c2 / (at.K_succ + s_c2),
 )
 eq_J_oa_c2 = Eq(
     J_oa_c2,
-    oa.a * oa.v_gluc * g_c2 / (oa.K_gluc + g_c2)
-    + (1 - oa.a) * oa.v_succ * s_c2 / (oa.K_succ + s_c2),
+    oa_c2_a * oa.v_gluc * g_c2 / (oa.K_gluc + g_c2)
+    + (1 - oa_c2_a) * oa.v_succ * s_c2 / (oa.K_succ + s_c2),
 )
